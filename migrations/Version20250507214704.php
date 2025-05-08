9cf7dbb95dee277b9f4aba92d6d602aad107e3e7
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-05-08 22:43:01 +0100

    RegistrationController

---
+++ migrations/Version20250507214704.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250507214704 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE playlist ADD CONSTRAINT FK_D782112DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_D782112DA76ED395 ON playlist (user_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE playlist DROP FOREIGN KEY FK_D782112DA76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_D782112DA76ED395 ON playlist
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP is_verified
+        SQL);
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -21,37 +20,28 @@ class RegistrationFormType extends AbstractType
                 'label' => 'Adresse email',
                 'attr' => [
                     'placeholder' => 'votre@email.com',
-                    'class' => 'form-control'
+                    'class' => 'form-control',
+                    'autocomplete' => 'email'
                 ]
             ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les mots de passe doivent correspondre.',
-                'first_options' => [
-                    'label' => 'Mot de passe',
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Veuillez entrer un mot de passe',
-                        ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                            'max' => 4096,
-                        ]),
-                    ],
-                    'attr' => [
-                        'placeholder' => '••••••',
-                        'class' => 'form-control'
-                    ]
-                ],
-                'second_options' => [
-                    'label' => 'Confirmer le mot de passe',
-                    'attr' => [
-                        'placeholder' => '••••••',
-                        'class' => 'form-control'
-                    ]
-                ],
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
                 'mapped' => false,
+                'attr' => [
+                    'placeholder' => '••••••',
+                    'class' => 'form-control',
+                    'autocomplete' => 'new-password'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
             ]);
     }
 
